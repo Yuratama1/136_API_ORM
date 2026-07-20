2fdb84ff0f12fa16b3f8b0af9d696e248c9068bc
Yuratama1 <yuratama661@gmail.com>
2026-07-20 10:33:02 +0700

    membuat, mengonfigurasi, dan menyalakan sebuah server web menggunakan framework

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const db = require('./models');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+    console.log(`Server started on port 30000`);
+});
